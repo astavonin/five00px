f74f984956e9e09c2070d5558629c5edfdbda8b9
Alexander Stavonin <alex@sysdev.me>
2016-03-03 19:15:34 +0800

    Moving User related functions to new location

--- client/user.go
+++ client/user.go
@@ -1,113 +1,202 @@
 // Package five00px provides ...
 package five00px
 
-// User 500px structure. See https://github.com/500px/api-documentation/blob/master/basics/formats_and_terms.md#full-format
-// for additional information.
-type User struct {
-	About                 string        `json:"about"`
-	Affection             int           `json:"affection"`
-	AllowSaleRequests     int           `json:"allow_sale_requests"`
-	AnalyticsCode         interface{}   `json:"analytics_code"`
-	Auth                  Auth          `json:"auth"`
-	Avatars               Avatars       `json:"avatars"`
-	Birthday              string        `json:"birthday"`
-	City                  string        `json:"city"`
-	Contacts              Contacts      `json:"contacts"`
-	Country               string        `json:"country"`
-	CoverURL              interface{}   `json:"cover_url"`
-	CustomLicensePrices   []interface{} `json:"custom_license_prices"`
-	Domain                string        `json:"domain"`
-	Email                 string        `json:"email"`
-	Equipment             Equipment     `json:"equipment"`
-	Firstname             string        `json:"firstname"`
-	FollowersCount        int           `json:"followers_count"`
-	Following             bool          `json:"following"`
-	FotomotoOn            bool          `json:"fotomoto_on"`
-	FriendsCount          int           `json:"friends_count"`
-	Fullname              string        `json:"fullname"`
-	GalleriesCount        int           `json:"galleries_count"`
-	ID                    int           `json:"id"`
-	InFavoritesCount      int           `json:"in_favorites_count"`
-	InviteAccepted        bool          `json:"invite_accepted"`
-	InvitePending         bool          `json:"invite_pending"`
-	Lastname              string        `json:"lastname"`
-	Locale                string        `json:"locale"`
-	PhotosCount           int           `json:"photos_count"`
-	PresubmitForLicensing interface{}   `json:"presubmit_for_licensing"`
-	RegistrationDate      string        `json:"registration_date"`
-	Sex                   int           `json:"sex"`
-	ShowNude              bool          `json:"show_nude"`
-	State                 string        `json:"state"`
-	StoreOn               bool          `json:"store_on"`
-	UpgradeStatus         int           `json:"upgrade_status"`
-	UpgradeStatusExpiry   string        `json:"upgrade_status_expiry"`
-	UpgradeType           int           `json:"upgrade_type"`
-	UploadLimit           int           `json:"upload_limit"`
-	UploadLimitExpiry     string        `json:"upload_limit_expiry"`
-	Username              string        `json:"username"`
-	UserpicHTTPSURL       string        `json:"userpic_https_url"`
-	UserpicURL            string        `json:"userpic_url"`
-	Usertype              int           `json:"usertype"`
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/Sirupsen/logrus"
+)
+
+func userBy(c *http.Client, dstPoint string, vals url.Values) (*User, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context":       "userBy",
+		"lookup_method": vals,
+	})
+
+	b, err := doCommand(c, dstPoint, http.MethodGet, vals)
+	if err != nil {
+		log.WithError(err).Warn("Failed to GET data")
+		return nil, ErrUserNotFound
+	}
+
+	var objmap map[string]*json.RawMessage
+	err = json.Unmarshal(b, &objmap)
+
+	if err != nil {
+		log.WithError(err).WithField("data", string(b)).
+			Error("Unable to unmarshall data")
+		return nil, err
+	}
+
+	var u User
+	err = json.Unmarshal(*objmap["user"], &u)
+	log.WithError(err).Info("Done")
+
+	return &u, err
 }
 
-// Equipment structure contains dictionary of a user's equipment
-type Equipment struct {
-	Camera []string `json:"camera"`
-	Lens   []string `json:"lens"`
-	Misc   []string `json:"misc"`
+// UserByID call returns User struct for a user specified by id. If id == 0
+// returns the profile information for the current user.
+func (f00 *Five00px) UserByID(id int) (*User, error) {
+	dstPoint := "users"
+	vals := url.Values{}
+	if id != 0 {
+		dstPoint += "/show"
+		vals.Add("id", strconv.Itoa(id))
+	}
+
+	return userBy(f00.c, dstPoint, vals)
 }
 
-// Auth structure contains dictionary of a user's social network authentications.
-type Auth struct {
-	Facebook     int `json:"facebook"`
-	GoogleOauth2 int `json:"google_oauth2"`
-	Twitter      int `json:"twitter"`
+// UserByName returns User struct for a user specified by name.
+func (f00 *Five00px) UserByName(name string) (*User, error) {
+
+	return userBy(f00.c, "users/show", url.Values{"username": {name}})
 }
 
-// Contacts structure contacts dictionary of user’s contacts, object
-type Contacts struct {
-	Facebook     string `json:"facebook"`
-	Facebookpage string `json:"facebookpage"`
-	Googleplus   string `json:"googleplus"`
-	Skype        string `json:"skype"`
-	Twitter      string `json:"twitter"`
-	Website      string `json:"website"`
+// UserByEmail returns User struct for a user specified by email.
+func (f00 *Five00px) UserByEmail(email string) (*User, error) {
+
+	return userBy(f00.c, "users/show", url.Values{"email": {email}})
 }
 
-// URLInfo is used in Avatars structure
-type URLInfo struct {
-	HTTP  string `json:"http"`
-	HTTPS string `json:"https"`
+// Friends call returns list of friends for a user specified by ID.
+func (f00 *Five00px) Friends(id int, p *Page) (*Friends, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context": "Friends",
+		"id":      id,
+		"page":    p,
+	})
+
+	b, err := doCommand(f00.c, "users/"+strconv.Itoa(id)+"/friends", http.MethodGet,
+		pageToVals(p))
+
+	if err != nil {
+		log.WithError(err).Warn("Failed to GET data")
+		return nil, err
+	}
+
+	var f Friends
+	err = json.Unmarshal(b, &f)
+	log.WithError(err).Info("Done")
+
+	return &f, err
 }
 
-// Avatars is a dictionary of different avatar sizes
-type Avatars struct {
-	Default URLInfo `json:"default"`
-	Large   URLInfo `json:"large"`
-	Small   URLInfo `json:"small"`
-	Tiny    URLInfo `json:"tiny"`
+// Followers call returns list of followers for a user specified by ID.
+func (f00 *Five00px) Followers(id int, p *Page) (*Followers, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context": "Followers",
+		"id":      id,
+		"page":    p,
+	})
+	b, err := doCommand(f00.c, "users/"+strconv.Itoa(id)+"/followers",
+		http.MethodGet, pageToVals(p))
+
+	if err != nil {
+		log.WithError(err).Warn("Failed to GET data")
+		return nil, err
+	}
+
+	var f Followers
+	err = json.Unmarshal(b, &f)
+	log.WithError(err).Info("Done")
+
+	return &f, err
 }
 
-// Friends structure is a list of friends for the specified user
-type Friends struct {
-	Users        []User `json:"friends"`
-	FriendsCount int    `json:"friends_count"`
-	FriendsPages int    `json:"friends_pages"`
-	Page         int    `json:"page"`
+// Search call returns list of users (up to one hundred) users from search
+// results for a specified search term
+func (f00 *Five00px) Search(term string, p *Page) (*Search, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context": "Search",
+		"term":    term,
+		"page":    p,
+	})
+	v := pageToVals(p)
+	v.Add("term", term)
+	b, err := doCommand(f00.c, "users/search", http.MethodGet, v)
+
+	if err != nil {
+		log.WithError(err).Warn("Failed to GET data")
+		return nil, ErrUserNotFound
+	}
+
+	var s Search
+	err = json.Unmarshal(b, &s)
+	log.WithError(err).Info("Done")
+
+	return &s, err
 }
 
-// Followers structure is a list of followers for the specified user
-type Followers struct {
-	Users          []User `json:"followers"`
-	FollowersCount int    `json:"followers_count"`
-	FollowersPages int    `json:"followers_pages"`
-	Page           int    `json:"page"`
+// AddFriend call adds new friend by user ID. Returns errors: ErrUserNotFound
+// and ErrUserAlreadyFriend
+func (f00 *Five00px) AddFriend(id int) (*User, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context": "DelFriend",
+		"id":      id,
+	})
+	b, err := doCommand(f00.c, "users/"+strconv.Itoa(id)+"/friends",
+		http.MethodPost, nil)
+
+	if err != nil {
+		var e00 five00Error
+		err = json.Unmarshal(b, &e00)
+		if err != nil {
+			log.WithError(err).WithField("data", string(b)).
+				Error("Unable to unmarshall data")
+			return nil, ErrInternal
+		}
+		log.WithField("status", strconv.Itoa(e00.Status)).
+			Info("server returns error")
+		switch e00.Status {
+		case http.StatusNotFound:
+			return nil, ErrUserNotFound
+		case http.StatusForbidden:
+			return nil, ErrUserAlreadyFriend
+		}
+	}
+
+	var f User
+	err = json.Unmarshal(b, &f)
+	log.WithError(err).Info("Done")
+	return &f, err
 }
 
-// Search structure (up to one hundred) users from search results for a specified search term.
-type Search struct {
-	Users       []User `json:"users"`
-	CurrentPage int    `json:"current_page"`
-	TotalPages  int    `json:"total_pages"`
-	TotalItems  int    `json:"total_items"`
+// DelFriend call deletes friend by user ID. Returns errors: ErrUserNotFound
+// and ErrUserNotFriend
+func (f00 *Five00px) DelFriend(id int) (*User, error) {
+	log := logrus.WithFields(logrus.Fields{
+		"context": "DelFriend",
+		"id":      id,
+	})
+
+	b, err := doCommand(f00.c, "users/"+strconv.Itoa(id)+"/friends",
+		http.MethodDelete, nil)
+
+	if err != nil {
+		var e00 five00Error
+		err = json.Unmarshal(b, &e00)
+		if err != nil {
+			log.WithError(err).WithField("buffer", string(b)).
+				Error("Unable to unmarshall data")
+			return nil, ErrInternal
+		}
+		log.WithField("status", strconv.Itoa(e00.Status)).
+			Info("server returns error")
+		switch e00.Status {
+		case http.StatusNotFound:
+			return nil, ErrUserNotFound
+		case http.StatusForbidden:
+			return nil, ErrUserNotFriend
+		}
+	}
+
+	var f User
+	err = json.Unmarshal(b, &f)
+	log.WithError(err).Info("Done")
+	return &f, err
 }
---
+++ client/user_types.go
@@ -0,0 +1,113 @@
+// Package five00px provides ...
+package five00px
+
+// User 500px structure. See https://github.com/500px/api-documentation/blob/master/basics/formats_and_terms.md#full-format
+// for additional information.
+type User struct {
+	About                 string        `json:"about"`
+	Affection             int           `json:"affection"`
+	AllowSaleRequests     int           `json:"allow_sale_requests"`
+	AnalyticsCode         interface{}   `json:"analytics_code"`
+	Auth                  Auth          `json:"auth"`
+	Avatars               Avatars       `json:"avatars"`
+	Birthday              string        `json:"birthday"`
+	City                  string        `json:"city"`
+	Contacts              Contacts      `json:"contacts"`
+	Country               string        `json:"country"`
+	CoverURL              interface{}   `json:"cover_url"`
+	CustomLicensePrices   []interface{} `json:"custom_license_prices"`
+	Domain                string        `json:"domain"`
+	Email                 string        `json:"email"`
+	Equipment             Equipment     `json:"equipment"`
+	Firstname             string        `json:"firstname"`
+	FollowersCount        int           `json:"followers_count"`
+	Following             bool          `json:"following"`
+	FotomotoOn            bool          `json:"fotomoto_on"`
+	FriendsCount          int           `json:"friends_count"`
+	Fullname              string        `json:"fullname"`
+	GalleriesCount        int           `json:"galleries_count"`
+	ID                    int           `json:"id"`
+	InFavoritesCount      int           `json:"in_favorites_count"`
+	InviteAccepted        bool          `json:"invite_accepted"`
+	InvitePending         bool          `json:"invite_pending"`
+	Lastname              string        `json:"lastname"`
+	Locale                string        `json:"locale"`
+	PhotosCount           int           `json:"photos_count"`
+	PresubmitForLicensing interface{}   `json:"presubmit_for_licensing"`
+	RegistrationDate      string        `json:"registration_date"`
+	Sex                   int           `json:"sex"`
+	ShowNude              bool          `json:"show_nude"`
+	State                 string        `json:"state"`
+	StoreOn               bool          `json:"store_on"`
+	UpgradeStatus         int           `json:"upgrade_status"`
+	UpgradeStatusExpiry   string        `json:"upgrade_status_expiry"`
+	UpgradeType           int           `json:"upgrade_type"`
+	UploadLimit           int           `json:"upload_limit"`
+	UploadLimitExpiry     string        `json:"upload_limit_expiry"`
+	Username              string        `json:"username"`
+	UserpicHTTPSURL       string        `json:"userpic_https_url"`
+	UserpicURL            string        `json:"userpic_url"`
+	Usertype              int           `json:"usertype"`
+}
+
+// Equipment structure contains dictionary of a user's equipment
+type Equipment struct {
+	Camera []string `json:"camera"`
+	Lens   []string `json:"lens"`
+	Misc   []string `json:"misc"`
+}
+
+// Auth structure contains dictionary of a user's social network authentications.
+type Auth struct {
+	Facebook     int `json:"facebook"`
+	GoogleOauth2 int `json:"google_oauth2"`
+	Twitter      int `json:"twitter"`
+}
+
+// Contacts structure contacts dictionary of user’s contacts, object
+type Contacts struct {
+	Facebook     string `json:"facebook"`
+	Facebookpage string `json:"facebookpage"`
+	Googleplus   string `json:"googleplus"`
+	Skype        string `json:"skype"`
+	Twitter      string `json:"twitter"`
+	Website      string `json:"website"`
+}
+
+// URLInfo is used in Avatars structure
+type URLInfo struct {
+	HTTP  string `json:"http"`
+	HTTPS string `json:"https"`
+}
+
+// Avatars is a dictionary of different avatar sizes
+type Avatars struct {
+	Default URLInfo `json:"default"`
+	Large   URLInfo `json:"large"`
+	Small   URLInfo `json:"small"`
+	Tiny    URLInfo `json:"tiny"`
+}
+
+// Friends structure is a list of friends for the specified user
+type Friends struct {
+	Users        []User `json:"friends"`
+	FriendsCount int    `json:"friends_count"`
+	FriendsPages int    `json:"friends_pages"`
+	Page         int    `json:"page"`
+}
+
+// Followers structure is a list of followers for the specified user
+type Followers struct {
+	Users          []User `json:"followers"`
+	FollowersCount int    `json:"followers_count"`
+	FollowersPages int    `json:"followers_pages"`
+	Page           int    `json:"page"`
+}
+
+// Search structure (up to one hundred) users from search results for a specified search term.
+type Search struct {
+	Users       []User `json:"users"`
+	CurrentPage int    `json:"current_page"`
+	TotalPages  int    `json:"total_pages"`
+	TotalItems  int    `json:"total_items"`
+}
